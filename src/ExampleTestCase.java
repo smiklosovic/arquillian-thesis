649226077a062b2b0ca8075fd7e732c42049582a
Stefan Miklosovic <smikloso@redhat.com>
2013-04-29 00:03:19 +0200

    JUnit section finished

---
+++ src/ExampleTestCase.java
@@ -0,0 +1,35 @@
+import net.miklosovic.junit.example.Adder;
+
+@RunWith(JUnit4.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class ExampleTestCase {
+
+    Adder adder;
+
+    @Rule
+    public Timeout globalTimeout =
+        new Timeout(1000);
+
+    @Before
+    public void beforeTest() {
+        adder = new Adder();
+    }
+
+    @AfterClass
+    public static void afterClass() { }
+
+    @Test
+    public void addTwoAndTwo() {
+        assertNotNull("adder is null!", adder);
+        assertThat(adder.addPositiveNumbers(2, 2),
+            is(4));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void notPositive() {
+        adder.addPositiveNumbers(-1, -2);
+        fail();
+    }
+
+    @Ignore @Test public void notPreparedYet() {}
+}
