4828f23a2aaaae9e38c1c238778722a19d0f6dfb
Stefan Miklosovic <smikloso@redhat.com>
2013-04-28 21:33:23 +0200

    JUnit preparation

---
+++ code/junit-examples/src/main/java/net/miklosovic/junit/example/Adder.java
@@ -0,0 +1,25 @@
+package net.miklosovic.junit.example;
+
+/**
+ * 
+ * @author Stefan Miklosovic
+ * 
+ */
+public class Adder {
+
+    /**
+     * Adds two positive number
+     * 
+     * @param a
+     * @param b
+     * @return sum of {@code a} and {@code b}
+     * @throws IllegalArgumentException when {@code a} or {@code b} is not positive
+     */
+    public int addPositiveNumbers(int a, int b) throws IllegalArgumentException {
+        if (a < 0 || b < 0) {
+            throw new IllegalArgumentException("Both arguments have to be positive.");
+        }
+        return a + b;
+    }
+
+}
---
+++ code/junit-examples/src/test/java/net/miklosovic/junit/example/test/ExampleTestCase.java
@@ -0,0 +1,49 @@
+package net.miklosovic.junit.example.test;
+
+import net.miklosovic.junit.example.Adder;
+
+import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.Timeout;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.junit.runners.MethodSorters;
+
+import static org.junit.Assert.*;
+
+@RunWith(JUnit4.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class ExampleTestCase {
+
+    Adder adder;
+
+    @Rule
+    public Timeout globalTimeout = new Timeout(10000);
+    
+    @Before
+    public void beforeTest() {
+        adder = new Adder();
+    }
+    
+    @AfterClass
+    public static void afterClass() { }
+    
+    @Test
+    public void addTwoAndTwo() {
+        assertNotNull("adder is null!", adder);
+        assertThat(adder.addPositiveNumbers(1, 2), is(3));
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void notPositive() {
+        adder.addPositiveNumbers(-1, -2);
+    }
+    
+    @Ignore @Test public void notPreparedYet() {}
+}
