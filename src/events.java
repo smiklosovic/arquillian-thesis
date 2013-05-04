216e573d8fc0904b3c5da98acc4baf464a371868
Stefan Miklosovic <smikloso@redhat.com>
2013-05-04 17:44:49 +0200

    Arquillian event model

---
+++ src/events.java
@@ -0,0 +1,10 @@
+// class to fire
+public class EventToFire {}
+
+// injecting of event used for firing
+@Inject Event<EventToFire> event;
+
+// actual  firing of the event
+public void observingMethod(@Observes SomeEvent e) {
+    event.fire(new EventToFire());
+}
