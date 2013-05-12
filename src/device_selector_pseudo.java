1cebd6114a39f8a8d4daf4fd3a75a3835cda7578
Stefan Miklosovic <smikloso@redhat.com>
2013-05-13 01:17:31 +0200

    added device selector pseudocode

---
+++ src/device_selector_pseudo.java
@@ -0,0 +1,20 @@
+if (we go to connect to physical device) {
+  get physical device
+  set physical device to injection point
+  fire AndroidDeviceReady event
+} else if (we go to connect to virtual device) {
+  get virtual device
+  set virtual device to injection point
+  fire AndroidDeviceReady event
+}
+// we do not have any instance of running real
+// or virtual device at this moment
+if (device name is not specified) {
+  generate random identifier as device name
+  mark name of device as generated
+}
+if (generated device name does not already exist) {
+  fire AndroidVirtualDeviceCreate event
+} else {
+  fire AndroidVirtualDeviceAvailable event
+}
