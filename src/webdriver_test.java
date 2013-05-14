58edf71409e00043a0e7cfcea671bf5bd4373847
Stefan Miklosovic <smikloso@redhat.com>
2013-05-15 01:11:43 +0200

    Test and Usage done

--- src/webdriver_test.java
+++ src/webdriver_test.java
@@ -1,16 +1,23 @@
-@RunWith(Arquillian.class) @RunAsClient
+@RunWith(Arquillian.class)
+@RunAsClient
 public class WebDriverTest {
-  @Deployment(name ="jboss")
+
+  @Deployment(name ="webapp")
   @TargetsContainer("jbossas")
   public static WebArchive createDeployment() {
     // By ShrinkWrap, assembly web app into WAR
   }
 
-  @Drone WebDriver driver;
-  @ArquillianResource URL deploymentUrl;
+  @Drone
+  WebDriver driver;
+ 
+  @ArquillianResource
+  URL deploymentUrl;
 
-  @Test @OperatesOnDeployment("jboss")
+  @Test
+  @OperatesOnDeployment("webapp")
   public void test01() {
-    driver.get(deploymentUrl); //tests
+    driver.get(deploymentUrl);
+    //tests
   }
 }
